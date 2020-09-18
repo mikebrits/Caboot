8401416312641c887b911fe4b4a72259f2e1d70f
Oliver Myatt <olivermyatt@rentalcars.com>
2020-09-18 11:18:07 +0100

    add grommet ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import React from "react";
+import { Grommet } from "grommet";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+
+const theme = {
+  global: {
+    font: {
+      family: "Roboto",
+      size: "14px",
+      height: "20px",
+    },
+  },
+};
+
+export default function App({ Component, pageProps }) {
+  return (
+      <Grommet theme={theme}>
+        <Head>
+          <link
+              href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+              rel="stylesheet"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Grommet>
+  );
 }
-
-export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {Heading} from "grommet";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Caboot</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Heading level={1}>
+          Welcome to Caboot
+        </Heading>
 
         <p className={styles.description}>
           Get started by editing{' '}
